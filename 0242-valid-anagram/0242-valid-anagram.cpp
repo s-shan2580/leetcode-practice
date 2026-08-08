09373fb32bd46c95ef0abe3d363215139cc17329
S_Shan <ushatiwary108@gmail.com>
2026-08-08 14:44:35 +0530

    Time: 0 ms (100%), Space: 9.6 MB (76.48%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     bool isAnagram(string s1, string t1) {
-        unordered_map<char,int>mpp;
-        if(s1.size()!=t1.size()){
-            return false;
+
+        int freq[26]={0};
+
+        if(s1.size()!=t1.size()) return false;
+
+        for(char s: s1){
+            freq[int(s-'a')]++;
         }
-        for(char s:s1){
-            mpp[s]++;
-        }
-        for(char t : t1){
-            if(mpp[t]>0){
-                mpp[t]--;
+
+        for(char t: t1){
+            if(freq[int(t-'a')]>0){
+                  freq[int(t-'a')]--;
             }
             else{
                 return false;
             }
+          
         }
+
         return true;
     }
 };
