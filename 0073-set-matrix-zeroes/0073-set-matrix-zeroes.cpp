a09290b02995517b9973006816140ff46e1a8c14
S_Shan <ushatiwary108@gmail.com>
2026-08-30 18:32:18 +0530

    Time: 0 ms (100%), Space: 20.9 MB (46.04%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,49 +4,26 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
 
-        int col0 = 0;
+        vector<bool> row(m, false);
+        vector<bool> col(n, false);
 
-        // Step 1: Store markers
+        // Find all zeroes
         for (int i = 0; i < m; i++) {
-
-            // Check first column
-            if (matrix[i][0] == 0)
-                col0 = 1;
-
-            for (int j = 1; j < n; j++) {
-
+            for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == 0) {
-                    // Mark row
-                    matrix[i][0] = 0;
-
-                    // Mark column
-                    matrix[0][j] = 0;
+                    row[i] = true;
+                    col[j] = true;
                 }
             }
         }
 
-        // Step 2: Use markers to set inner matrix
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-
-                if (matrix[i][0] == 0 ||
-                    matrix[0][j] == 0) {
-
+        // Set required rows and columns to zero
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] || col[j]) {
                     matrix[i][j] = 0;
                 }
             }
-        }
-
-        // Step 3: Handle first row
-        if (matrix[0][0] == 0) {
-            for (int j = 0; j < n; j++)
-                matrix[0][j] = 0;
-        }
-
-        // Step 4: Handle first column
-        if (col0 == 1) {
-            for (int i = 0; i < m; i++)
-                matrix[i][0] = 0;
         }
     }
 };
