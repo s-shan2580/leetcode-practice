ea0b91d05c0705b0f449dfd352a6c12ae8241775
S_Shan <ushatiwary108@gmail.com>
2026-09-20 22:08:44 +0530

    Time: 0 ms (100%), Space: 17.7 MB (17.39%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,47 +1,48 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& arr, int key) {
-        
-        int start=0;
-        int end=arr.size()-1;
-        int first=-1;
-        int last=-1;
-        while(start<=end){
-            int mid= start + (end-start)/2 ;
-            if(arr[mid]==key){
-                first=mid;
-                end=mid-1;
-            }
-            else if(arr[mid]<key){
-                start=mid+1;
+    vector<int> searchRange(vector<int>& arr, int x) {
+        int first = -1;
+        int last = -1;
+        int n = arr.size();
+
+        int start = 0;
+        int end = n - 1;
+
+        if (n == 0) {
+            return {-1, -1};
+        }
+
+        while (start <= end) { // finding first occurance
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == x) {
+                first = mid;
+                end = mid - 1;
+            } 
+            else if(arr[mid] > x) {
+                end = mid - 1;
             }
             else{
-                 end=mid-1;
+                start = mid + 1;
             }
-            
-        }
-         start=0;
-         end=arr.size()-1;
-
-        while(start<=end){
-            int mid= start + (end-start)/2 ;
-            if(arr[mid]==key){
-                last=mid;
-                start=mid+1;
-            }
-            else if(arr[mid]<key){
-                start=mid+1;
-            }
-            else {
-                 end=mid-1;
-            }
-            
         }
 
-        // vector<int>nums(2);
-        // nums[0]=first;
-        // nums[1]=last;
-        return {first,last};
-        
+         start = 0;
+         end = n - 1;
+
+        while (start <= end) { // finding last occurance
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == x) {
+                last = mid;
+                start = mid + 1;
+            }
+            else if(arr[mid] > x) {
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+
+        return {first , last};
     }
 };
