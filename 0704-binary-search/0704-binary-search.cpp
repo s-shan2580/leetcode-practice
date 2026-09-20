28eef0545517a06011aa8ec48a2eac801842ad05
S_Shan <ushatiwary108@gmail.com>
2026-09-20 18:02:00 +0530

    Time: 0 ms (100%), Space: 31.4 MB (45.84%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
-    int search(vector<int>& arr, int key) {
-        int n=arr.size();
+
+    int search(vector<int>& arr, int x){
+        int n= arr.size();
         int start=0;
         int end=n-1;
+        return b_search(arr, start, end, x);
+    }
+
+    int b_search(vector<int>& arr, int start, int end, int x) {
+
         while(start<=end){
-            int mid= start + (end-start)/2 ;
-            if(arr[mid]==key){
+            int mid= start + (end-start)/2;
+            if(arr[mid]==x){
                 return mid;
             }
-            else if(arr[mid]<key){
-                start=mid+1;
+            else if(arr[mid]<x){
+               start= mid+1;
+               return b_search(arr, start, end, x);
             }
             else{
-                end=mid-1;
+                end= mid-1;
+                return b_search(arr, start, end, x);
             }
         }
+
         return -1;
     }
-    
 };
