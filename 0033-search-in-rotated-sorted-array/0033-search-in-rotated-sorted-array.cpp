b295c376888af7541a582cc91bb95f7a4340fd1d
S_Shan <ushatiwary108@gmail.com>
2026-09-21 15:28:26 +0530

    Time: 0 ms (100%), Space: 15.2 MB (69.93%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -2,57 +2,41 @@ class Solution {
 public:
     int search(vector<int>& arr, int x) {
         int n = arr.size();
-        int dip = n - 1;
-
-        if (n == 1) {
-            if (arr[0] == x)
-                return 0;
-            else
-                return -1;
-        }
-
-        for (int i = 0; i < n - 1; i++) { // Finding the dip
-            if (arr[i] > arr[i + 1]) {
-                dip = i;
-                break;
-            } else {
-                continue;
-            }
-        }
 
         int start = 0;
-        int end = dip;
+        int end = n - 1;
 
-        while (start <= end) { // finding x in first half
+        while (start <= end) {
+
             int mid = start + (end - start) / 2;
+
             if (arr[mid] == x) {
                 return mid;
-            } else if (arr[mid] > x) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
+            }
+
+            // Left half is sorted
+            else if (arr[start] <= arr[mid]) {
+
+                if (arr[start] <= x && x < arr[mid]) {
+                    end = mid - 1;
+                }
+                else {
+                    start = mid + 1;
+                }
+            }
+
+            // Right half is sorted
+            else {
+
+                if (arr[mid] < x && x <= arr[end]) {
+                    start = mid + 1;
+                }
+                else {
+                    end = mid - 1;
+                }
             }
         }
-
-        start= dip+1;
-        end= n-1;
-
-        while (start <= end) { // finding x in second half
-            int mid = start + (end - start) / 2;
-            if (arr[mid] == x) {
-                return mid;
-            } else if (arr[mid] > x) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-        }
-
 
         return -1;
-
-
-
-
     }
 };
