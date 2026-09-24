e2f65459cbb7562dcd4f3861bfc80b46c781b75e
S_Shan <ushatiwary108@gmail.com>
2026-09-24 14:37:04 +0530

    Time: 29 ms (17.34%), Space: 70.4 MB (98.79%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,51 +1,39 @@
 class Solution {
 public:
-
     int checkadj(vector<int>& arr, int days, int m, int k){
-        int adj_ok = 0; int count = 0;
-        
-        for(int i=0; i<arr.size(); i++){
-            
-            if(arr[i]<=days ){
-                adj_ok++ ;
-                if(adj_ok == k){
-                    adj_ok = 0;
+        int streak = 0, count = 0;
+        for(int i = 0; i < arr.size(); i++){
+            if(arr[i] <= days){
+                streak++;
+                if(streak == k){
                     count++;
+                    streak = 0;
                 }
+            } else {
+                streak = 0;
             }
-            else{
-                adj_ok = 0;
-            }
-
-           
         }
-       
         return count >= m;
     }
 
     int minDays(vector<int>& bloomDay, int m, int k) {
+        long long totalflowers = (long long)m * k;
         int n = bloomDay.size();
-        long long totalflowers = 1LL * m * k;
-
-        if (totalflowers > n || (n==1 && totalflowers > n)) return -1;
+        if (totalflowers > n) return -1;
 
         int start = *min_element(bloomDay.begin(), bloomDay.end());
         int end = *max_element(bloomDay.begin(), bloomDay.end());
         int ans = end;
 
-        while(start<=end){
-            
-            int mid = start + (end-start)/2;
-
+        while(start <= end){
+            int mid = start + (end - start) / 2;
             if(checkadj(bloomDay, mid, m, k)){
                 ans = mid;
                 end = mid - 1;
-            }
-            else{
+            } else {
                 start = mid + 1;
             }
         }
-
         return ans;
     }
 };
